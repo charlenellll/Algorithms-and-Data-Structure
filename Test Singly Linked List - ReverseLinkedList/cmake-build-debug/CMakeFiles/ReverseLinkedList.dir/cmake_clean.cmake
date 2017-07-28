178902bb0846a63d78221a00343fc57ad453a229
Jialing Liao <charleneliaojl@gmail.com>
2017-07-28 20:54:09 +0800

    Test my own linked list.

---
+++ Test Singly Linked List - ReverseLinkedList/cmake-build-debug/CMakeFiles/ReverseLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReverseLinkedList.dir/main.cpp.o"
+  "ReverseLinkedList.pdb"
+  "ReverseLinkedList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReverseLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
